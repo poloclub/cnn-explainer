8a6c5a9b822bf0d186ef048cd44941cb91d99909
Jay Wong <xiao.hk1997@gmail.com>
2019-11-04 16:18:04 -0500

    Change indent

--- src/Explainer.svelte
+++ src/Explainer.svelte
@@ -1,16 +1,16 @@
 <script>
-	import Overview from './Overview.svelte';
-	import Layerview from './Layerview.svelte';
-	import Detailview from './Detailview.svelte';
+  import Overview from './Overview.svelte';
+  import Layerview from './Layerview.svelte';
+  import Detailview from './Detailview.svelte';
 
-	// Enum to control the displaying view
-	const View = {
-		OVERVIEW: 'overview',
-		LAYERVIEW: 'layerview',
-		DETAILVIEW: 'detailview'
-	};
+  // Enum to control the displaying view
+  const View = {
+    OVERVIEW: 'overview',
+    LAYERVIEW: 'layerview',
+    DETAILVIEW: 'detailview'
+  };
 
-	let mainView = View.OVERVIEW;
+  let mainView = View.OVERVIEW;
 </script>
 
 <style>
