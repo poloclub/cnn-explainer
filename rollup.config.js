07d496cefc4e603d0ce612296e93773150e189e4
Jay Wong <xiao.hk1997@gmail.com>
2019-11-08 19:35:09 -0500

    Restructure assets

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/bundle.js'
+		file: 'public/assets/js/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -22,7 +22,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('public/assets/css/bundle.css');
 			}
 		}),
 
