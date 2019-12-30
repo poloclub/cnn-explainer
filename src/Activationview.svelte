45d04319589b3ab60fda27045ff58b3087a28e00
rturko3 <rturko3@gatech.edu>
2019-12-30 14:04:39 -0500

    add exit logic to detailed views

--- src/Activationview.svelte
+++ src/Activationview.svelte
@@ -1,10 +1,13 @@
 <script>
 	import ActivationAnimator from './ActivationAnimator.svelte';
+  import { createEventDispatcher } from 'svelte';
 
   export let input;
   export let output;
   export let dataRange;
-  
+
+  const dispatch = createEventDispatcher();
+  let isExited = false;
   let isPaused = false;
   
   function handleClickPause() {
@@ -14,26 +17,46 @@
   function handlePauseFromInteraction(event) {
     isPaused = event.detail.text;
   }
+
+  function handleClickX() {
+    isExited = true;
+    dispatch('message', {
+      text: isExited
+    });
+  }
 </script>
 
-<div class="container">
-  <div style="margin-top: 2%; margin-bottom: 2%;" class="box">
-    <div class="has-text-left">
-      <button style="margin-bottom: 1%" 
-        class="button is-small"
-        id="pause-button"
-        class:is-activated={isPaused}
-        on:click={handleClickPause}>
-        <span class="icon">
-          {@html isPaused ? '<i class="fas fa-play"></i>' : '<i class="fas fa-pause"></i>'}
-        </span>
-      </button>  
-    </div>
-    <div class="columns is-centered is-vcentered">
-      <ActivationAnimator on:message={handlePauseFromInteraction} 
-        image={input} output={output} isPaused={isPaused}
-        dataRange={dataRange}/>
+{#if !isExited}
+  <div class="container">
+    <div style="margin-top: 2%; margin-bottom: 2%;" class="box">
+      <div class="columns is-centered">
+        <div class="column has-text-left">
+          <button style="margin-bottom: 1%" 
+            class="button is-small"
+            id="pause-button"
+            class:is-activated={isPaused}
+            on:click={handleClickPause}>
+            <span class="icon">
+              {@html isPaused ? '<i class="fas fa-play"></i>' : '<i class="fas fa-pause"></i>'}
+            </span>
+          </button> 
+        </div>
+        <div class="column has-text-right"> 
+          <button style="margin-bottom: 1%" 
+            class="button is-small is-centered"
+            id="x-button"
+            on:click={handleClickX}>
+            <span class="icon">
+              <i class="fas fa-times"></i>
+            </span>
+          </button>
+        </div>  
+      </div>
+      <div class="columns is-centered is-vcentered">
+        <ActivationAnimator on:message={handlePauseFromInteraction} 
+          image={input} output={output} isPaused={isPaused}
+          dataRange={dataRange}/>
+      </div>
     </div>
   </div>
-
-</div>
+{/if}
