652017b2415f0c339c2da89c021f74ab44a327e3
Jay Wong <xiao.hk1997@gmail.com>
2020-01-01 09:48:56 -0500

    Resize play button and change detail view card layout

--- src/Poolview.svelte
+++ src/Poolview.svelte
@@ -39,6 +39,32 @@
   }
 </script>
 
+<style>
+  .control-pannel {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .play-button {
+    margin-right: 3px;
+  }
+
+  .control-button {
+    color: gray;
+    font-size: 15px;
+    opacity: 0.4;
+    cursor: pointer;
+  }
+
+  .control-button:hover {
+    opacity: 0.8;
+  }
+
+  .box {
+    padding: 5px 15px 10px 15px;
+  }
+</style>
+
 {#if !isExited}
   <div class="container">
 
@@ -57,28 +83,19 @@
         </div>
       </div>
     </div> -->
-    <div style="margin-top: 2%; margin-bottom: 2%;" class="box">
-      <div class="columns is-centered">
-        <div class="column has-text-left">
-          <button style="margin-bottom: 1%" 
-            class="button is-small"
-            id="pause-button"
-            class:is-activated={isPaused}
-            on:click={handleClickPause}>
-            <span class="icon">
-              {@html isPaused ? '<i class="fas fa-play"></i>' : '<i class="fas fa-pause"></i>'}
-            </span>
-          </button> 
+    <div class="box">
+
+      <div class="control-pannel">
+        <div class="play-button control-button" on:click={handleClickPause}>
+          {@html isPaused ?
+            '<i class="fas fa-play-circle play-icon"></i>' :
+            '<i class="fas fa-pause-circle"></i>'}
         </div>
-        <div class="column has-text-right"> 
-          <button style="margin-bottom: 1%" 
-            class="delete is-small"
-            arial-label="close"
-            id="x-button"
-            on:click={handleClickX}>
-          </button>
-        </div>  
+        <div class="delete-button control-button" on:click={handleClickX}>
+            <i class="fas control-icon fa-times-circle"></i>
+        </div>
       </div>
+
       <div class="columns is-centered is-vcentered">
         <PoolAnimator on:message={handlePauseFromInteraction} 
           kernelLength={kernelLength} image={input} output={outputFinal} 
