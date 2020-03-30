cb222525ccba0b2fb230be59e9bdacf5c94ad9aa
rturko3 <rturko3@gatech.edu>
2020-03-30 14:02:38 -0400

    update article to reflect interface changes

--- src/Article.svelte
+++ src/Article.svelte
@@ -41,6 +41,10 @@
     margin: 16px 0;
   }
 
+  #description p img {
+    vertical-align: middle;
+  }
+
   #description .figure-caption {
     font-size: 13px;
     margin-top: 5px;
@@ -56,6 +60,10 @@
     color: #555;
     font-size: 17px;
     line-height: 1.6;
+  }
+
+  #description ol li img {
+    vertical-align: middle;
   }
     
   #description a:hover, 
@@ -100,7 +108,7 @@
     </p>
     <h4>Input Layer</h4>
     <p>
-    	The input layer (leftmost layer) represents the input image into the CNN.  Because we use RGB images as input, the input layer has three channels, corresponding to the red, green, and blue channels, respectively, which are shown in this layer. Use the color scale when you click on the <img class="icon" align="top" src="/assets/figures/eye.png" alt="eye icon" /> icon above to display detailed information (on this layer, and others).
+    	The input layer (leftmost layer) represents the input image into the CNN.  Because we use RGB images as input, the input layer has three channels, corresponding to the red, green, and blue channels, respectively, which are shown in this layer. Use the color scale when you click on the <img class="is-rounded" width="12%" height="12%" src="/assets/figures/network_details.png" alt="network details icon"/> icon above to display detailed information (on this layer, and others).
     </p>
     <h4>Convolutional Layers</h4>
     <p>
@@ -177,11 +185,11 @@
  
     <h2>Interactive features</h2>
     <ol>
-		<li><strong>Upload your own image</strong> via url to understand how your image is classified into the 10 classes.  By analyzing at the neurons throughout the network, you can understand the activations maps and extracted features.  <em>TODO: insert image here showing where/how a user can upload an image</em></li>
-		<li><strong>Change the activation map colorscale</strong> to better understand the impact of activations at different levels of abstraction by adjusting <img class="is-rounded" width="15%" height="15%" align="top" src="/assets/figures/heatmap_scale.png" alt="heatmap"/>.</li>
-		<li><strong>Understand network details</strong> such as layer dimensions and colorscales by clicking the <img class="icon" align="top" src="/assets/figures/eye.png" alt="eye icon"/> icon.</li>
-		<li><strong>Simulate network operations</strong> by clicking the <img class="icon" align="top" src="/assets/figures/play_button.png" alt="play icon"/> button or interact with the layer slice in the <em>Detail View</em> by hovering over portions of the input or output to understand the mappings and underlying operations.</li>
-	</ol> 
+		<li><strong>Upload your own image</strong> by selecting <img class="icon is-rounded" src="/assets/figures/upload_image_icon.png" alt="upload image icon"/> to understand how your image is classified into the 10 classes.  By analyzing the neurons throughout the network, you can understand the activations maps and extracted features.</li>
+		<li><strong>Change the activation map colorscale</strong> to better understand the impact of activations at different levels of abstraction by adjusting <img class="is-rounded" width="12%" height="12%" src="/assets/figures/heatmap_scale.png" alt="heatmap"/>.</li>
+		<li><strong>Understand network details</strong> such as layer dimensions and colorscales by clicking the <img class="is-rounded" width="12%" height="12%" src="/assets/figures/network_details.png" alt="network details icon"/> icon.</li>
+		<li><strong>Simulate network operations</strong> by clicking the <img class="icon is-rounded" src="/assets/figures/play_button.png" alt="play icon"/> button or interact with the layer slice in the <em>Detail View</em> by hovering over portions of the input or output to understand the mappings and underlying operations.</li>
+    </ol> 
 
     <h2>How is CNN Explainer implemented?</h2>
     <p>
