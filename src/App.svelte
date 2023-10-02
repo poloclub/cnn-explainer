663bbd24b7736b317b013ddbaf4b445948c9736b
Jay Wang <jay@zijie.wang>
2023-10-02 08:39:44 -0400

    Add recommender overlay
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script>
-	import Explainer from './Explainer.svelte';
-	import Header from './Header.svelte';
+  import Explainer from './Explainer.svelte';
+  import Header from './Header.svelte';
 </script>
 
 <style>
 </style>
 
 <div id="app-page">
-	<Header />
-	<Explainer />
+  <recommender-overlay
+    my-brand="CNN Explainer"
+    brands-to-ignore="CNN 101"
+    recommendation-count="10"
+    similar-candidate-count="15"
+    position-left="30"
+    display-delay="90000"
+    homepage-url="https://poloclub.github.io/"
+  />
+  <Header />
+  <Explainer />
 </div>
